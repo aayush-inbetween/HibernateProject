d6b07e72a3db085e3a7efa8f04c506d2c1602731
sagar-inbetween <sagar.jaisinghani@inbetween.co.in>
2015-07-31 20:03:18 +0530

    comit

--- src/test/java/com/ib/test/onetomany/notmanaged/manageteam/UpdateTeam.java
+++ src/test/java/com/ib/test/onetomany/notmanaged/manageteam/UpdateTeam.java
@@ -1,9 +1,16 @@
 package com.ib.test.onetomany.notmanaged.manageteam;
 
+import java.util.List;
+
+import javax.annotation.Resource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.testng.annotations.Test;
 
 import com.ib.onetomany.notmanaged.eager.connector.TeamConnector;
+import com.ib.onetomany.notmanaged.eager.entity.Player;
+import com.ib.onetomany.notmanaged.eager.entity.Team;
 import com.ib.test.configuration.ApplicationContextConfiguration;
 
 
@@ -12,4 +19,32 @@ class UpdateTeam
 {
 	@Autowired
 	TeamConnector teamconnector;
+	
+	
+	
+
+	@Resource(name = "createdTeamsIds")
+	List<Long> createdTeamsIds;
+
+
+	@Test(priority = 1)
+	public void createTeam() {
+
+		Team team = new Team();
+		team.setName("New-Zealand");
+		
+		team = teamconnector.saveTeam(team);
+		createdTeamsIds.add(team.getId());
+	}
+
+
+	@Test(priority = 2)
+	public void execute() {
+
+		Team team = teamconnector.getTeam(createdTeamsIds.get(0));
+		team.setName("Bangladesh");
+		
+		teamconnector.saveTeam(team);
+		
+	}
 }
