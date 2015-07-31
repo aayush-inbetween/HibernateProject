73bc18726353eda1fe5f1c6a4e81527cca47b3d6
hitesh-inbetween <hitesh.hotchandani@inbetween.co.in>
2015-07-31 18:24:41 +0530

    getAll

--- src/test/java/com/ib/test/onetomany/notmanaged/manageplayer/ListAll.java
+++ src/test/java/com/ib/test/onetomany/notmanaged/manageplayer/ListAll.java
@@ -2,8 +2,6 @@ package com.ib.test.onetomany.notmanaged.manageplayer;
 
 import java.util.List;
 
-import javax.annotation.Resource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
@@ -14,30 +12,34 @@ import com.ib.onetomany.notmanaged.eager.entity.Player;
 import com.ib.test.configuration.ApplicationContextConfiguration;
 
 @ContextConfiguration(classes = ApplicationContextConfiguration.class)
-public class ListAll extends AbstractTestNGSpringContextTests{
+public class ListAll extends AbstractTestNGSpringContextTests {
 
 	@Autowired
 	PlayerConnector playerConnector;
-	
-	@Resource(name = "createdPlayerIds")
-	List<Long> createdPlayerIds;
-	
+
+	/*
+	 * @Resource(name = "createdPlayerIds") List<Long> createdPlayerIds;
+	 */
 
 	@Test(priority = 1)
-	public void createPlayer(){
+	public void createPlayer() {
 		Player player = new Player();
 		player.setName("Rahul Dravid");
 		player.setAge(35);
-		player = playerConnector.savePlayer(player);
-		
+		playerConnector.savePlayer(player).getId();
+
 		Player player2 = new Player();
 		player2.setName("Sourav Ganguly");
 		player2.setAge(35);
 		playerConnector.savePlayer(player2);
 	}
-	
+
 	@Test(priority = 2)
-	public void execute(){
-		playerConnector.getAllPlayers();
+	public void execute() {
+		List<Player> player = playerConnector.getAllPlayers();
+		System.out.println("ID\tName\tAge\tTeam");
+		for(int i=0;i<player.size();i++) {
+			System.out.println(player.get(i).getId()+"\t"+player.get(i).getName()+"\t"+player.get(i).getAge()+"\t"+player.get(i).getTeam());
+		}
 	}
 }
